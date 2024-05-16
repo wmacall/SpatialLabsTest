1f64aa2f7beb1ee3a335db61f6c24581aa59a602
wmacall <macallwalter@gmail.com>
2024-05-16 10:28:57 -0600

    ✨ Add app colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import {Typography} from './src/components';
+import {COLORS} from './src/constants';
 
 const App = () => {
   return (
-    <View>
+    <View style={{backgroundColor: COLORS.PRIMARY, flex: 1}}>
       <SafeAreaView />
       <Typography variant="h3">Hello World!</Typography>
       <Typography variant="h4">Hello World!</Typography>
