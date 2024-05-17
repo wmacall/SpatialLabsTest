648dfe4b3ae8ca696e9aae0306157904546b74da
wmacall <macallwalter@gmail.com>
2024-05-17 16:25:51 -0600

    ✨ Finish home screen

--- src/screens/App/Home/Home.screen.styles.ts
+++ src/screens/App/Home/Home.screen.styles.ts
@@ -1,12 +1,48 @@
 import {StyleSheet} from 'react-native';
 import {COLORS} from '../../../constants';
+import {theme} from '../../../utils';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.PRIMARY,
   },
+  containerHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: theme.scaleWidth(24),
+    marginBottom: theme.scaleHeight(24),
+    marginTop: theme.scaleHeight(8),
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageUser: {
+    width: theme.scaleWidth(64),
+    height: theme.scaleWidth(64),
+    borderRadius: theme.scaleWidth(32),
+  },
+  defaultImage: {
+    width: theme.scaleWidth(64),
+    height: theme.scaleWidth(64),
+    borderRadius: theme.scaleWidth(32),
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.DARK_SECONDARY,
+  },
+  containerInfo: {
+    marginLeft: theme.scaleWidth(18),
+  },
   contentContainerStyle: {
+    alignSelf: 'center',
+    alignItems: 'flex-start',
+  },
+  containerButton: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
     alignItems: 'center',
   },
 });
--- src/screens/App/Home/Home.screen.tsx
+++ src/screens/App/Home/Home.screen.tsx
@@ -1,23 +1,67 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
-import {AccountCard} from '../../../components';
+import {FlatList, Image, TouchableOpacity, View} from 'react-native';
+import {AccountCard, Button, Icon, Typography} from '../../../components';
 import {useSelector} from 'react-redux';
-import {getSocialMedia} from '../../../store';
+import {getProfileSelector, getSocialMedia} from '../../../store';
+import DotBlack from '../../../assets/img/dot_black.png';
 import styles from './Home.screen.styles';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {theme} from '../../../utils';
+import {useNavigation} from '@react-navigation/native';
+import {AppStackRoutes} from '../../../routes';
 
 export const HomeScreen = () => {
   const socialMedia = useSelector(getSocialMedia);
+  const {username, bio, photo} = useSelector(getProfileSelector);
+  const {bottom} = useSafeAreaInsets();
+  const {navigate} = useNavigation();
+
+  const handleNavigateToProfile = () => {
+    navigate(AppStackRoutes.PROFILE);
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.containerHeader}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            onPress={handleNavigateToProfile}
+            activeOpacity={0.6}>
+            {photo ? (
+              <Image
+                style={styles.imageUser}
+                resizeMode="cover"
+                source={{uri: photo}}
+              />
+            ) : (
+              <View style={styles.defaultImage}>
+                <Icon name="UserIcon" />
+              </View>
+            )}
+          </TouchableOpacity>
+          <View style={styles.containerInfo}>
+            <Typography variant="h6">{username}</Typography>
+            <Typography variant="small">{bio}</Typography>
+          </View>
+        </View>
+        <Image source={DotBlack} resizeMode="contain" />
+      </View>
       <FlatList
         numColumns={2}
         contentContainerStyle={styles.contentContainerStyle}
-        data={[...socialMedia, ...socialMedia, ...socialMedia, ...socialMedia]}
+        data={socialMedia}
         keyExtractor={(item, index) => `${item.name}-${index}`}
         renderItem={({item, index}) => (
           <AccountCard isOdd={index % 2 === 0} {...item} />
         )}
       />
+      <View
+        style={[
+          styles.containerButton,
+          {bottom: bottom + theme.scaleHeight(27)},
+        ]}>
+        <Button icon="AddIcon" buttonColor="GREEN" variant="rounded" />
+      </View>
     </View>
   );
 };
